c5d7d52d8584709c2a9c6a85bd3bf46599be8c6e
Gabriel Amorim <ricksamann@gmail.com>
2024-03-06 00:59:44 -0300

    req 01 e 02

---
+++ src/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.get('/', (_req, res) => {
+  res.send();
+});
--- src/utils/fs/readJsonData.js
+++ src/utils/fs/readJsonData.js
@@ -1,5 +1,8 @@
-const readJsonData = async (_path) => {
+const fs = require('node:fs/promises');
 
+const readJsonData = async (_path) => {
+  const data = await fs.readFile(_path, { encoding: 'utf-8' });
+  return JSON.parse(data);
 };
 
 module.exports = readJsonData;
